7ec92593ed28d58306b0c831840da7c5e7db2019
Sukhpaul Nijjar <sukhpaul.nijjar@gmail.com>
2017-07-23 10:25:54 -0700

    Works through _.reject

--- src/underbar.js
+++ src/underbar.js
@@ -81,8 +81,22 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var newArr = [];
+    _.each(collection, function(item) {
+      if(test(item)){
+        newArr.push(item);
+      }
+    });
+    return newArr;
   };
+/*
+filter_.filter(list, predicate, [context]) Alias: select 
+Looks through each value in the list, returning an array of all the values that pass a truth test (predicate).
 
+var evens = _.filter([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
+=> [2, 4, 6]
+
+*/
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
