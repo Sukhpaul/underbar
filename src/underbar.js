e2b3e7a77b4eb9c507a16d8ee9baacdfc7d643e0
Sukhpaul Nijjar <sukhpaul.nijjar@gmail.com>
2017-07-22 17:20:34 -0700

    Complete last and each

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -37,7 +38,9 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    return n === undefined ? array[array.length - 1] : n === 0 ? [] : array.slice(-n, array.length);
   };
+
 
   // Call iterator(value, key, collection) for each element of collection.
   // Accepts both arrays and objects.
@@ -45,6 +48,18 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if(Array.isArray(collection)){
+      for(var i = 0; i < collection.length; i++){
+        iterator(collection[i], i, collection);
+      };
+    } else {
+      for(var key in collection){
+      //   iterator(key);
+      //Object.keys(collection).forEach(function (key) {
+        iterator(collection[key], key, collection);
+         // do something with key or value
+      };
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
